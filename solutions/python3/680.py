867ec423035b006ff590b7ac058d0765bfb7800a
Udayan Porpatham <uporpatham@icloud.com>
2024-01-25 14:08:27 -0500

    Update 680.py

--- solutions/python3/680.py
+++ solutions/python3/680.py
@@ -1,3 +1,4 @@
+#Solution 1
 class Solution:
     def validPalindrome(self, s):
         """
@@ -25,3 +26,13 @@ class Solution:
                 memo[(l, r, cnt)] = dfs(l + 1, r - 1, cnt)
                 return memo[(l, r, cnt)]
         return dfs(0, len(s) - 1, 0)
+#Solution2 
+class Solution:
+    def validPalindrome(self, s: str) -> bool:
+        b: int = len(s) - 1
+        for i in range(len(s)//2):
+            if s[i] != s[b-i]:
+                pos1 = s[:i] + s[i+1:]
+                pos2 = s[:b-i] + s[b-i+1:]
+                return pos1==pos1[::-1] or pos2==pos2[::-1]
+        return True
