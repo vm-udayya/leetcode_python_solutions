1b6869eaf8452469cc1867f97b35c2a22a4f86f2
Udayan Porpatham <uporpatham@icloud.com>
2024-01-25 14:02:06 -0500

    Update 1249.py

--- solutions/python3/1249.py
+++ solutions/python3/1249.py
@@ -1,3 +1,4 @@
+# Solution 1 
 class Solution:
     def minRemoveToMakeValid(
         self, s: str, res: str = "", l: str = "(", r: str = ")", b: int = 0
@@ -11,3 +12,21 @@ class Solution:
                 res += c
             res, s, l, r, b = "", res[::-1], r, l, 0
         return s
+#Solution 2
+class Solution:
+    def minRemoveToMakeValid(self, s: str) -> str:
+        stack = []
+        s = list(s)
+        for i in range(len(s)):
+            if s[i] == '(':
+                stack.append(i)
+            elif s[i] == ')':
+                if stack:
+                    stack.pop()
+                else:
+                    s[i] = ''
+            
+        for i in stack:
+            s[i] = ''
+
+        return ''.join(s)
